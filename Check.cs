72d06934e1c4e6b838c0d86112bebcf1713ba3cd
damiantomczyk32 <damiantomczyk32@gmail.com>
2019-07-13 11:26:35 +0200

    Hamming distance implementation

---
+++ Check.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammingDistance
+{
+    class Check
+    {
+        public int Distance(string ciagA,string ciagB)
+        {
+            int counter = 0;
+            ciagA = ciagA.Trim();
+            ciagB = ciagB.Trim();
+            if(ciagA.Length==ciagB.Length)
+            {
+                for(int i=0;i<ciagA.Length;i++)
+                {
+                    if (ciagA[i] != ciagB[i])
+                        counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check text = new Check();
+            Console.WriteLine(text.Distance("!AA10","AAB10"));
+            Console.ReadKey();
+        }
+    }
+}
